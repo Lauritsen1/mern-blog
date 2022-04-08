269384b676afc1d14a759c281e42e86e01dd2664
Lauritsen1 <43439056+Lauritsen1@users.noreply.github.com>
2022-04-08 16:11:41 +0200

    Home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,18 +1,33 @@
+import { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { getAllPosts, reset } from '../features/posts/postSlice';
+
 import PostPreview from '../components/PostPreview';
 
 function Home() {
-    return (
-        <div className='grid gap-6 grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 md:auto-cols-auto'>
-            <PostPreview />
-            <PostPreview />
-            <PostPreview />
-            <PostPreview />
-            <PostPreview />
-            <PostPreview />
-            <PostPreview />
-            <PostPreview />
-        </div>
-    )
+    const dispatch = useDispatch();
+
+    const { posts, isLoading, isError, message } = useSelector(
+        (state) => state.posts
+    );
+
+    useEffect(() => {
+        if (isError) {
+            console.log(message);
+        }
+
+        dispatch(getAllPosts());
+
+        return () => {
+            dispatch(reset());
+        };
+    }, [isError, message, dispatch]);
+
+    const postList = posts.map((post) => (
+        <PostPreview key={post._id} post={post} />
+    ));
+
+    return <div className='grid gap-6 grid-cols-1'>{posts && postList}</div>;
 }
 
 export default Home;
