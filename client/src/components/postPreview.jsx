38e9081e47a3f554fbf6c6d99c55396b94c3411d
Lauritsen1 <43439056+Lauritsen1@users.noreply.github.com>
2022-04-08 16:11:09 +0200

    Redesign postpreview

--- client/src/components/postPreview.jsx
+++ client/src/components/postPreview.jsx
@@ -1,32 +1,50 @@
 import { Link } from 'react-router-dom';
 
-function PostPreview() {
+function PostPreview({ post }) {
     return (
-        <div className='shadow-lg rounded-lg border flex flex-col justify-between p-6 aspect-4/3'>
-
-            <div className='flex flex-col gap-2 mb-6'>
-                <h3 className='text-indigo-600 font-medium text-sm'>Fitness</h3>
-                <h2 className='font-bold text-lg leading-6'>Someone breaks record</h2>
-                <p className='font-medium text-gray-500 text-sm leading-6'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis rerum dolorem qui nulla eius neque quia blanditiis sed minima fuga.</p>
-            </div>
-
-            <div className='flex justify-between'>
-                <div className='flex items-center gap-2'>
-                    <div className='h-3/4 rounded-full aspect-square bg-indigo-600 text-indigo-300 font-bold flex justify-center items-center text-sm'>
-                        RL
-                    </div>
-                    <div>
-                        <p className='font-medium text-sm whitespace-nowrap'>Rasmus Lauritsen</p>
-                        <p className='text-sm'>Feb 12, 2020</p>
-                    </div>
+        <div className='flex flex-col justify-between w-full p-6 mx-auto bg-white rounded-lg shadow-md border max-w-xl'>
+            <div>
+                <div className='flex items-center justify-between'>
+                    <span className='text-sm font-light text-gray-600'>
+                        {new Date(post.createdAt).toLocaleDateString('da-DK')}
+                    </span>
+                    <Link
+                        to='/'
+                        className='px-2 py-1 text-sm font-medium text-gray-100 transition-colors duration-200 transform bg-blue-600 hover:bg-blue-500 rounded cursor-pointer'>
+                        Design
+                    </Link>
                 </div>
 
-                <Link className='font-medium self-end text-indigo-600' to='/'>Read<i className="bi bi-arrow-right ml-1"></i></Link>
-
+                <div className='mt-2'>
+                    <Link
+                        to={`/post/${post._id}`}
+                        href='#'
+                        className='text-2xl font-bold text-gray-700 hover:text-gray-600 hover:underline'>
+                        {post.title}
+                    </Link>
+                    <p className='mt-2 text-gray-600'>
+                        {post.subTitle
+                            ? post.subTitle
+                            : post.text.substring(1, 200)}
+                    </p>
+                </div>
             </div>
 
+            <div className='flex items-center justify-between mt-4'>
+                <Link to='/' href='#' className='text-blue-600 hover:underline'>
+                    Read more
+                </Link>
+
+                <div className='flex items-center'>
+                    <Link
+                        to='/'
+                        className='font-medium text-gray-700 cursor-pointer'>
+                        Rasmus Lauritsen
+                    </Link>
+                </div>
+            </div>
         </div>
-    )
+    );
 }
 
 export default PostPreview;
