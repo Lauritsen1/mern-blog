361730797bb9e6215bf53f750b2ee57910776439
Lauritsen1 <43439056+Lauritsen1@users.noreply.github.com>
2022-04-08 21:03:47 +0200

    static foulder

--- server/app.js
+++ server/app.js
@@ -13,7 +13,6 @@ const app = express();
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors());
 
 app.use('/api/posts', require('./routes/postRoutes'));
